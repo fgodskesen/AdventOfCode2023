404356ad62d91a360dc57aa36d7ce62b57b1b8b3
Flemming Godskesen <f.godskesen@gmail.com>
2023-12-01 12:05:36 +0100

    day 1 part 1

---
+++ task1.ps1
@@ -0,0 +1,18 @@
+# get all lines in input
+$data = Get-Content $PSScriptRoot\input.txt
+
+$totalSum = 0
+foreach ($line in $data) {
+    #On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+    $reverse = $line[-1.. - ($line.Length)] -join ""
+
+    # find first digit in string
+    $d1 = [regex]::Match($line, "\d").Value
+    $d2 = [regex]::Match($reverse, "\d").Value
+
+    $thisNumber = [int]("{0}{1}" -f $d1, $d2)
+
+    "{0} `t`t {1} `t`t {2}" -f $thisNumber, $line, $reverse
+    $totalSum += $thisNumber
+}
+Write-Host "Final number : $totalSum"
